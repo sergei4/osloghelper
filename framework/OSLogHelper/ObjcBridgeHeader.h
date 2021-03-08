e919f571b553b8f3cb428b96e367d5577587d2db
Mikhail Eremkin <eremkin@devexperts.com>
2021-03-08 15:07:03 +0300

    Copy ActivityStreamSPI.h and OSLogStream

---
+++ framework/OSLogHelper/ObjcBridgeHeader.h
@@ -0,0 +1,12 @@
+//
+//  ObjcBridgeHeader.h
+//  OSLogHelper
+//
+//  Created by Mikhail Eremkin on 08.03.2021.
+//
+
+#ifndef ObjcBridgeHeader_h
+#define ObjcBridgeHeader_h
+#import "ActivityStreamSPI.h"
+
+#endif /* ObjcBridgeHeader_h */
